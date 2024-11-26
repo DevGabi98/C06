5576f853e0dad39a2351792df801d26f12480827
gabdev98 <gabdev98@MSI.>
2024-11-26 18:21:03 +0100

    ex03 corretto

--- ex03/ft_sort_params.c
+++ ex03/ft_sort_params.c
@@ -7,19 +7,17 @@ int	ft_strlen(char *str)
 	int	i;
 
 	i = 0;
-	while (*str)
+	while (str[i])
 		i++;
 	return (i);
 }
 
 int	ft_strcmp(char *str1, char *str2)
 {
-	int	i;
-
-	i = 0;
-	while(*str1 && *str2 && *str1 == *str2)
+	while(*str1 && (*str1 == *str2))
 	{
-		i++;
+		str1++;
+		str2++;
 	}
 	return (*str1 - *str2);
 }
@@ -36,12 +34,12 @@ int main(int argc, char **argv)
     int i;
     int j;
 
-    i = 1; //i = 1 perché arg[0] è il nome del programma e non ci interessa ordinarlo
-    while (i < argc) //finché non abbiamo considerato tutti gli argomenti.
+    i = 1;
+    while (i < argc)
     {
-        j = i + 1; // impostiamo j = i + 1 cosicché verifichi tutti gli argomenti cge vengono dopo 
+        j = i + 1; 
         while (j < argc)
-        {   //se il confronto tra i due argomenti è > 0, significa che argv[i] viene dopo argv[j] in ordine alfabetico, allora fa swap e li invert
+        {
             if (ft_strcmp(argv[i], argv[j]) > 0)
             {
                 swap(&argv[i], &argv[j]);
